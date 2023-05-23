2a4bb975d30daf1bcde58b3cab2ef64a8d35ae8f
Bobby Kumar <bobby.kumar.1905@gmail.com>
2023-05-23 19:53:03 +0530

    Auth verified routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,33 @@
 import { Routes, Route, useLocation } from 'react-router-dom';
 import { MantineProvider } from '@mantine/core';
 import { Notifications } from '@mantine/notifications';
-import { useRef } from 'react';
 import { PageNotFound } from './pages/PageNotFound';
-import { Navbar } from './components/common/Navbar';
-import { Footer } from './components/common/Footer';
 import { Landing } from './pages/Landing';
 import { AuthPage } from './pages/AuthPage';
 import { Waitlist } from './pages/Waitlist';
 import { GoogleAuthRedirect } from './components/Auth/components/Google/GoogleAuthRedirect';
 import { ProfilePage } from './pages/ProfilePage';
-import { validRoutes } from './utils/constants/ValidRoutes';
+import { AuthVerificationLayout } from './utils/constants/AuthVerification';
+import { AppLayout } from './components/layouts/AppLayout';
 
 const App = () => {
-  const { pathname } = useLocation();
-
   return (
     <>
       <MantineProvider withGlobalStyles withNormalizeCSS>
         <Notifications position="top-right" zIndex={999} />
-        {validRoutes.includes(pathname) ? <Navbar /> : null}
+
         <Routes>
-          <Route path="/" element={<Landing />} />
-          <Route path="/auth" element={<AuthPage />} />
-          <Route path="/waitlist" element={<Waitlist />} />
-          <Route path="/oauth/google/callback" element={<GoogleAuthRedirect />} />
-          <Route path="/profile" element={<ProfilePage />} />
+          <Route path="/" element={<AppLayout />}>
+            <Route index element={<Landing />} />
+            <Route path="auth" element={<AuthPage />} />
+            <Route path="waitlist" element={<Waitlist />} />
+            <Route path="oauth/google/callback" element={<GoogleAuthRedirect />} />
+            <Route element={<AuthVerificationLayout />}>
+              <Route path="profile" element={<ProfilePage />} />
+            </Route>
+          </Route>
           <Route path="*" element={<PageNotFound />} />
         </Routes>
-        {validRoutes.includes(pathname) ? <Footer /> : null}
       </MantineProvider>
     </>
   );
