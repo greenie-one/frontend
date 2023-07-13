7ba9598959fec449efa5905c63410aa4f3871cc6
Swanand-Wagh <swanandwagh7@gmail.com>
2023-07-13 20:13:11 +0530

    misc

--- src/components/auth/components/google/GoogleAuthRedirect.tsx
+++ src/components/auth/components/google/GoogleAuthRedirect.tsx
@@ -4,16 +4,20 @@ import { useSearchParams } from 'react-router-dom';
 import { useLocalStorage } from '@mantine/hooks';
 import { authApiList } from '../../../../assets/api/ApiList';
 import { HttpClient } from '../../../../utils/generic/httpClient';
+import { useAuthContext } from '../../context/AuthContext';
 
 export const GoogleAuthRedirect = () => {
   const [searchParams] = useSearchParams();
-  const [authTokens, setAuthTokens] = useLocalStorage<AuthTokens>({
+  const { setForceRender } = useAuthContext();
+
+  const [, setAuthTokens] = useLocalStorage<AuthTokens>({
     key: 'auth-tokens',
   });
 
   const getAuthTokens = async () => {
     if (searchParams) {
       const code = searchParams.get('code');
+
       if (code) {
         const res = await HttpClient.callApi<AuthTokens>({
           url: `${authApiList.googleCallback}`,
@@ -21,9 +25,9 @@ export const GoogleAuthRedirect = () => {
           query: { code: code },
         });
 
-        console.log(res);
         if (res.ok) {
           setAuthTokens(res.value);
+          setForceRender((prev) => !prev);
           window.close();
         } else {
           console.error(res.error.code);
--- src/components/auth/components/google/GoogleButton.tsx
+++ src/components/auth/components/google/GoogleButton.tsx
@@ -1,11 +1,7 @@
 import { useState } from 'react';
 import { Button } from '@mantine/core';
 import { useLocalStorage } from '@mantine/hooks';
-import {
-  showErrorNotification,
-  showLoadingNotification,
-  showSuccessNotification,
-} from '../../../../utils/functions/showNotification';
+import { showErrorNotification, showLoadingNotification } from '../../../../utils/functions/showNotification';
 import { HttpClient } from '../../../../utils/generic/httpClient';
 import { authApiList } from '../../../../assets/api/ApiList';
 import { useAuthContext } from '../../context/AuthContext';
@@ -36,7 +32,6 @@ const GoogleButton = () => {
 
     if (res.ok) {
       window.open(res.value?.redirectUrl, '_blank');
-      showSuccessNotification({ title: 'Success !', message: '' });
     } else {
       showErrorNotification(res.error.code);
     }
